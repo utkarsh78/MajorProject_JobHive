c2025338c0c279242e6953292baf478f2341c90b
spranjal56 <spranjal56@gmail.com>
2023-05-16 02:04:04 +0530

    frontEndTesting Done

---
+++ frontend/src/App.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+import Welcome from './component/Welcome'
+import video from "./component/video.mp4";
+
+// app.js test
+test("renders the landing page", () => {
+  render(<App />);
+});
+
+//Welcome Component test
+describe("Welcome component", () => {
+  test("renders without error", () => {
+    const { getByTestId } = render(<Welcome />);
+
+    // Select the video element using getByTestId
+    const videoElement = getByTestId("welcome-video");
+    expect(videoElement).toBeInTheDocument();
+  });
+});
+// npm install --save-dev @testing-library/jest-dom
